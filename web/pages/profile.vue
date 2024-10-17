3eea369d536e1527348679a4d45097de3c69c2df
GabrielReds13 <speedckprod4007@gmail.com>
2024-10-17 14:35:58 -0400

    Feat: Added Profile Page

---
+++ web/pages/profile.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col items-center gap-4 p-4 w-full h-full">
+    <!-- title -->
+    <h1 class="text-left text-2xl font-bold w-full">Perfil</h1>
+
+    <!-- profile -->
+    <div class="flex flex-row gap-4 w-full items-center">
+        <!-- profile photo -->
+        <img class="w-[6rem] h-[6rem] bg-gray-200 rounded-lg">
+        </img>
+    
+        <!-- user data -->
+        <div>
+            <h1 class="text-xl font-bold">User Name</h1>
+            <p class="text-gray-500 text-sm">user@gmail.com</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
