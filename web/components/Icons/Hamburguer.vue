e8c92bf187c79925e2758b227275045cc3197cc9
GabrielReds13 <speedckprod4007@gmail.com>
2024-10-24 01:40:10 -0300

    Fix: Ajust Screen and Icons
    Added new illustration and icon from header and 'create new account' screen and ajust profile page, switch content for show some user progress

---
+++ web/components/Icons/Hamburguer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <svg width="" height="" viewBox="0 0 17 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M15.6334 1.06342H1.06342M15.6334 7.06342H1.06342M15.6334 13.0634H1.06342" stroke="white" stroke-width="2.12679" stroke-linecap="round"/>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ web/pages/progress.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtLayout name="header-page">
+    <!-- title -->
+    <h1 class="text-left text-2xl font-bold w-full">Progresso</h1>
+
+    <div class="grid grid-cols-2 gap-4 w-full items-center">
+        <!-- cards -->
+        <VCProgressCard :name="'Concluídas'" :items="[]"/>
+        <VCProgressCard :name="'Em progresso'" :items="[]"/>
+        <VCProgressCard class="col-span-2" :name="'Total'" :items="[]"/>
+    </div>
+
+    <!-- today progress -->
+    <div class="flex flex-col items-center justify-center gap-2 mt-10">
+      <IllustrationsThroply class="w-[10rem]"/>
+
+      <p class="text-xl leading-4 text-center">
+        <span class="text-[#9E71FF] font-bold">Parabéns!</span> <br>
+        <span class="text-gray-500 text-sm">Você concluiu 5 tarefas hoje.</span> <br>
+      </p>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
