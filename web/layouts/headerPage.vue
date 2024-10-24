02d87aece340da7f53b0073b895efbfad40f24e8
GabrielReds13 <speedckprod4007@gmail.com>
2024-10-24 00:49:38 -0300

    Test Build Problem 1

--- web/layouts/headerPage.vue
+++ web/layouts/headerPage.vue
@@ -18,9 +18,10 @@
               transition="slide-y-transition"
               class="-show-overflow"
             >
-              <template #activator="enable">
+              <template #activator="{enable, button}">
                 <ProfilePhoto 
                   class="w-8 h-8 rounded-full"
+                  v-bind="button"
                   v-on="enable"
                 />
               </template>
